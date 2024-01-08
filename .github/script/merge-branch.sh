420fa7aa26a19ad0974b53e90db54416289d47cf
Shinsuke Higashiyama aka Cinzeng Zia <94058150+sinsukehlab@users.noreply.github.com>
2024-01-09 01:52:30 +0900

    Merge before update to step 5 (#57)
    * Create a script to merge branches
    
    Run the script to avoid conflicts
    
    * Correct the script path
    
    * Delete the instruction to resolve conflicts
    
    Now the conflicts are avoided!

---
+++ .github/script/merge-branch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Make sure this file is executable
+# chmod +x .github/script/merge-branch.sh
+
+# USAGE: This script is used to merge a branch into another branch
+
+# BACKGROUND: This operation is required to avoid conflicts between branches.
+
+# Setup commiter identity
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+# Merge branch
+echo "If branch $branch2 exists, merge branch origin $branch1 into branch $branch2"
+if git show-ref --quiet refs/heads/$branch2
+then
+  git checkout $branch2
+  git merge origin $branch1
+  git push origin $branch2
+else
+  echo "Branch $branch2 does not exist"
+fi
